d8f96f41c0dd3b6b7c0a759f2a15affe75cd3815
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-03-06 09:52:52 -0500

    b

--- js/script.js
+++ js/script.js
@@ -5,20 +5,16 @@ $(document).ready(function (e) {
     //generación automática de posts
     var postsHTML = "";
 
-    for (var post of posts) {
+    for(var post of posts){
         postsHTML += `<div class="post">
-            <div class="image-col">
-                <div class="image-container">
-                    <img class="image" src="${post.photo}" alt="foto ${post.name}">
-                </div>
+            <div class="image-container">
+                <img class="image" src="${post.photo}" alt="foto ${post.name}">
             </div>
-            <div class="content-col">
-                <div class="content">
-                    <h3 class="title">${post.name}</h3>
-                    <br><br>
-                    <p class="description">${post.description}</p>
-                </div>
-            </div>           
+            <div class="content">
+                <h3 class="title">${post.name}</h3>
+                <br><br>
+                <p class="description">${post.description}</p>
+            </div>
         </div>`;
     }
 
@@ -49,9 +45,9 @@ $(document).ready(function (e) {
         $(".gb").show();
     })
 
-    $("#myInput").on("keyup", function () {
+    $("#myInput").on("keyup", function() {
         var value = $(this).val().toLowerCase();
-        $("#myDIV .marco").filter(function () {
+        $("#myDIV .marco").filter(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
