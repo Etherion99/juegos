c64dce7149d2b74e77f6a7ee21e5063aac5e2886
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-03-06 08:53:19 -0500

    Update script.js
    script llenado dinamico posts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function (e) {
+    //filtros
+    var filters = ["category", "year", "studio", "console"];
+
+    //generación automática de posts
+    var postsHTML = "";
+
+    for(var post of posts){
+        postsHTML += `<div class="post">
+            <div class="image">
+                <img src="${post.photo}" alt="foto ${post.name}">
+            </div>
+            <div class="content">
+                <h3 class="title">${post.name}</h3>
+                <br>
+                <p class="description">${post.description}</p>
+            </div>
+        </div>`;
+    }
+
+    $("#posts").html(postsHTML);
+
 
     $("#gb_button, .gb_button").click(function () {
         $(".ds").hide();
